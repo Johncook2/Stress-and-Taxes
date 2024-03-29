c3d6aad057e8a86d371f1af2f1c714c8f5957668
Johncook2 <126199075+Johncook2@users.noreply.github.com>
2024-03-29 15:12:54 +0100

    Renaming

--- StressandTaxes/Loader.cs
+++ StressandTaxes/Loader.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace WinchModTemplate
+namespace StressandTaxes
 {
 	public class Loader
 	{
@@ -9,8 +9,8 @@ namespace WinchModTemplate
 		/// </summary>
 		public static void Initialize()
 		{
-			var gameObject = new GameObject(nameof(WinchModTemplate));
-			gameObject.AddComponent<WinchModTemplate>();
+			var gameObject = new GameObject(nameof(StressandTaxes));
+			gameObject.AddComponent<StressandTaxes>();
 			GameObject.DontDestroyOnLoad(gameObject);
 		}
 	}
---
+++ StressandTaxes/StressandTaxes.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace StressandTaxes
+{
+	public class StressandTaxes : MonoBehaviour
+	{
+		public void Awake()
+		{
+			WinchCore.Log.Debug($"{nameof(StressandTaxes)} has loaded!");
+		}
+	}
+}
