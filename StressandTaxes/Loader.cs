223af4f7c690cc5f843aa8df9d57798ec44fa549
Johncook2 <126199075+Johncook2@users.noreply.github.com>
2024-04-13 20:02:31 +0200

    Removed? Loader.cs

--- StressandTaxes/Loader.cs
+++ StressandTaxes/Loader.cs
@@ -1,17 +1,1 @@
-﻿using UnityEngine;
 
-namespace StressandTaxes
-{
-	public class Loader
-	{
-		/// <summary>
-		/// This method is run by Winch to initialize your mod
-		/// </summary>
-		public static void Initialize()
-		{
-			var gameObject = new GameObject(nameof(StressandTaxes));
-			gameObject.AddComponent<StressandTaxes>();
-			GameObject.DontDestroyOnLoad(gameObject);
-		}
-	}
-}
