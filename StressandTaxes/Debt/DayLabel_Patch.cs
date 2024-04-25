c39dc30b4beb3877029798d50c283ede0462cb9a
Johncook2 <126199075+Johncook2@users.noreply.github.com>
2024-04-25 20:45:37 +0200

    Fixes and Taxes
    Made it so the mod actually loads by doing some code scavenging from the other mods and added taxes to the mod. Yay!

---
+++ StressandTaxes/Debt/DayLabel_Patch.cs
@@ -0,0 +1,23 @@
+using System;
+using HarmonyLib;
+
+namespace StressandTaxes.Debt.Patches
+{
+	// Token: 0x02000004 RID: 4
+	[HarmonyPatch(typeof(DayLabel), "Update")]
+	public static class DayLabel_Update_Patch
+	{
+		// Token: 0x06000007 RID: 7 RVA: 0x00002C74 File Offset: 0x00000E74
+
+		[HarmonyPrefix]
+		static void Prefix(DayLabel __instance)
+		{
+			if (GameManager.Instance.Time.Day != __instance.prevDay)
+			{
+				GameManager.Instance.GameConfigData.greaterMarrowDebt = GameManager.Instance.GameConfigData.greaterMarrowDebt * (decimal)1.2;
+			}
+;
+		}
+	}
+
+}
---
+++ StressandTaxes/Loader.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace StressandTaxes
+{
+    public class Loader
+    {
+        /// <summary>
+        /// This method is run by Winch to initialize your mod
+        /// </summary>
+        public static void Initialize()
+        {
+            new Harmony("com.Johncook.StressandTaxes").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
